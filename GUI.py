8c79ff5b926b2960deb2fbc2562374483222d2a4
ty <tymars345678@aim.com>
2019-11-03 13:07:02 -0500

    FINAL FINAL FINAL

--- GUI.py
+++ GUI.py
@@ -6,6 +6,7 @@ import LTAT
 window = Tk()
 window.title("Lets Take A Trip")
 window.geometry('350x300')
+
 lbl = Label(window, text="Starting Location")
 lbl.grid(column=0, row=0)
 start = Entry(window,width=30)
@@ -14,14 +15,25 @@ lbl = Label(window, text="Ending Location")
 lbl.grid(column=0, row=1)
 end = Entry(window,width=30)
 end.grid(column=1, row=1)
-def forget(event):
-    event.widget.pack_forget()
+
+
+img = PhotoImage(file = r"accuweather.png") 
+img = img.subsample(10, 10) 
+label = Label(image=img)
+label.place(x=40, y=100, relwidth=1, relheight=1)
+
+
+
 def clicked():
-    #res = "Welcome to " + txt.get()
+
 
     startLoc = start.get()
     endLoc = end.get()
-    
+    lbl = Label(window, text="\n\n")
+    lbl.grid(column=1, row=3)
+    lblP = Label(window, text="      When should you leave?       \n                                 ")
+    lblP.grid(column=1, row=4)
+
     if(startLoc!="" and endLoc !=""):
         startLoc.rstrip()
         startLoc.lstrip()
@@ -30,15 +42,17 @@ def clicked():
 
         lbl = Label(window, text="Finding safest time to depart...")
         lbl.grid(column=1, row=2)
+        window.update()
+
         waitTime = LTAT.main(startLoc,endLoc)
         
         if(waitTime==0):
-            pText = ("The best time for you to leave for " +str(endLoc)+" would be now.")
+            pText = ("The best time for you to leave for\n " +str(endLoc)+" would be NOW.")
         else:
-            pText = "The best time for you to leave for " +str(endLoc)+" would be in "+str(waitTime)+" hour"+ ("s"if waitTine>1 else "")
+            pText = "The best time for you to leave for\n " +str(endLoc)+" would be in "+str(waitTime)+" HOUR"+ ("S"if waitTine>1 else "")
 
-        lbl = Label(window, text=pText)
-        lbl.grid(column=1, row=4)
+        
+        lblP.configure(text = pText)
 
 
 btn = Button(window, text="Search", command=clicked)
