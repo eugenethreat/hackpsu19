c7d92b06111086d94eb8f0c685ec87006f0f130a
unknown <eugryooene@gmail.com>
2019-11-03 05:38:59 -0500

    FIXING MINUTES TO HOURS TO ACTUALLY INCLUDE HOURS

--- directionalReturn.py
+++ directionalReturn.py
@@ -61,11 +61,30 @@ def legsToDict(passedDict):
         endLoc = passedDict['routes'][0]['legs'][0]['steps'][x]['end_location']
         
         duration.pop("value")
+        print(duration)
         time = duration.get("text")
         
         timeSplit = time.split(" ")
-        timeMinutes = int(timeSplit[0])
-        timeHours = timeMinutes / 60 
+        print(timeSplit)
+
+        if(timeSplit[1] == "hour"):
+            timeMinutes = int(timeSplit[2])
+            timeHours = timeMinutes / 60
+            timeHours = timeHours + 1
+            duration["text"] = timeHours 
+
+        elif(timeSplit[1] == "hours"):
+            timeMinutes = int(timeSplit[2])
+            amtOfRawHours = int(timeSplit[0])
+            timeHours = timeMinutes / 60
+            timeHours = timeHours + 1 * amtOfRawHours
+            duration["text"] = timeHours 
+
+        else:
+            timeMinutes = int(timeSplit[0])
+            timeHours = timeMinutes / 60 
+            duration["text"] = timeHours 
+
         #cleaning up output; getting rid of "Values", and converting minutes to hours. 
         '''
         more verbose: getting rid of "values", then fetching the time. Covnerting that time 
