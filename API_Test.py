59eb27dad41cce9acfaddd3243402291e752d0b6
unknown <you@eugenexps.com>
2019-11-03 01:56:52 -0500

    adding list outut

--- API_Test.py
+++ API_Test.py
@@ -1,7 +1,6 @@
 #ACCUWEATHER API KEY - 5u1ZRpMU9yQ2urfUL5cU1a3N2fFw3zAU
 import requests
 
-
 from latLongGrabber import latLongGrabber
 from latLongGrabber import stripper
 
@@ -21,6 +20,7 @@ API_Key = 'jjK2n1LcGnGF7Nrouf8tD29fjVzyfxOM'
 aw_geosite = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=5u1ZRpMU9yQ2urfUL5cU1a3N2fFw3zAU&q='+location[0] + '%2C%20' + location[1]
 response = requests.get("http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=jjK2n1LcGnGF7Nrouf8tD29fjVzyfxOM&q=40.7933949%2C-77.8600012")
 
-print(response.json())
+x = response.json() #a dictionary of key 
+print(x['Key'])
 
 #Run location key through forecast
---
+++ directionalReturn.py
@@ -0,0 +1,81 @@
+#GOOGLE API KEY - AIzaSyBjkt7bJVDGfg0eInZv4lTOMx6NCxTBVgs
+#ACCUWEATHER API KEY - 5u1ZRpMU9yQ2urfUL5cU1a3N2fFw3zAU
+
+#USE THIS FILE 
+
+global location
+
+import json
+import googlemaps
+import pprint
+import requests
+from datetime import datetime
+
+def directinonalReturn(origin, destination):
+    key = 'AIzaSyBjkt7bJVDGfg0eInZv4lTOMx6NCxTBVgs'
+    gmaps = googlemaps.Client(key=key)
+
+    orig = "State"+"College"+"PA"
+    dest = "New"+"York"+"City"+"NY"
+
+    #mapped = gmaps.directions(orig, dest) shit library
+
+    '''https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=YOUR_API_KEY'''
+
+    route = 'https://maps.googleapis.com/maps/api/directions/json?origin='+ orig +'&destination=' + dest + '&key=' + key
+    response = requests.get(route)
+        #raw API call
+
+    '''the route info is contained in the param 'legs' ''' 
+    routeAsJson = response.json() #a dictionary of key 
+
+    #print(routeAsJson['routes'])
+    jsonAsDict = jsonToObject(routeAsJson)
+    coordsAndDuration = legsToDict(jsonAsDict)
+
+    print(coordsAndDuration)
+
+
+def jsonToObject(passedJson):
+    jsonToStr = json.dumps(passedJson)
+    jsonAsDict = json.loads(jsonToStr)
+    #print(type(jsonToStr))
+    #print(type(jsonAsDict))
+
+    return jsonAsDict
+
+def legsToDict(passedDict):
+    #print(passedDict['routes'][0]['legs'][0]['distance']) 
+        #access the lat and long of the first value 
+    
+    stepList = []
+    length_key = len(passedDict['routes'][0]['legs'][0]['steps'])  # length of the list stored at `'key'` ...
+    length_key = length_key 
+    #print("lk: ",length_key)
+
+    for x in range(length_key):
+        leg1 = passedDict['routes'][0]['legs'][0]['steps'][x]
+        duration = passedDict['routes'][0]['legs'][0]['steps'][x]['duration']
+        endLoc = passedDict['routes'][0]['legs'][0]['steps'][x]['end_location']
+        
+        valuesToAdd = [endLoc, duration]
+
+        ''' 
+        21 ayaya:  {'distance': 
+            {'text': '197 ft', 'value': 60}, 
+            'duration': {'text': '1 min', 'value': 19}, 
+            'end_location': {'lat': 40.7124773, 'lng': -74.0062007}, 
+            'html_instructions': 'Turn <b>right</b> onto <b>Steve Flanders Square</b><div style="font-size:0.9em">Restricted usage road</div>', 
+            'maneuver': 'turn-right', 
+            'polyline': {'points': 'yqnwFrfubMWLA?C@A@A@?@A?[bAEH'}, 
+            'start_location': {'lat': 40.7121262, 'lng': -74.0056991}, '
+             travel_mode': 'DRIVING'}
+        '''
+        stepList.append(valuesToAdd)
+
+        print("returned, done") 
+        return stepList
+
+#aaa
+directinonalReturn("State College, PA", "New York City, NY")
+  
